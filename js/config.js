8827f087fb9d0af34c7c56c9d6b9d4224ce0655a
Foluso Ogunsakin <folusomaine@gmail.com>
2023-07-15 13:05:21 +0000

    config updated: Sat Jul 15 13:05:21 UTC 2023

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://qr2tqfmjj6.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://1xkwyl5n15.execute-api.us-east-1.amazonaws.com',
     }
 };
