1e8a9873b9b7638860d5bdabf11bac3396972941
Foluso Ogunsakin <folusomaine@gmail.com>
2023-07-17 21:05:45 +0000

    config updated: Mon Jul 17 21:05:44 UTC 2023

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_lAQoOIOqy',
-        userPoolClientId: '2rqo8tbe5r7n6gslgsp9rqmvuv',
+        userPoolId: 'us-east-1_owEp1kOxp',
+        userPoolClientId: '3kmnadea0q81sm6mdm28ugqpjs',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://1xkwyl5n15.execute-api.us-east-1.amazonaws.com/prod',
+        invokeUrl: 'https://w06hfywzn6.execute-api.us-east-1.amazonaws.com/prod',
     }
 };
